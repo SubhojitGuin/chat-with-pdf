004002912702c52d364934e854a2059db86e2acf
Subhojit Guin <subhojitguin2004@gmail.com>
2025-02-07 20:05:35 +0530

    update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
+from langchain_community.callbacks import get_openai_callback
 
 import langchain
 langchain.verbose = False
